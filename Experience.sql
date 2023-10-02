b1e2b73da96401a5c6c243013df65111c6d75ee8
chaseboykin <140556718+chaseboykin@users.noreply.github.com>
2023-10-01 20:43:00 -0500

    Update Experience.sql

--- Experience.sql
+++ Experience.sql
@@ -1,13 +1,30 @@
-SELECT circuits.name, races.name, date, constructors.name, results.time, fastestlap,
-wins, forename, surname
-FROM circuits
+--Who has been the most dominant team since 2000?
+
+SELECT constructors.name, constructorstandings.wins
+FROM constructors
+JOIN constructorstandings
+ON constructors.constructorid = constructorstandings.constructorid
 JOIN races
-ON circuits.circuitid = races.circuitid
-JOIN results
-ON races.raceid = results.raceid
+ON constructorstandings.raceid = races.raceid
+WHERE date > '2000-01-01'
+GROUP BY constructors.name, constructorstandings.wins
+ORDER BY constructorstandings.wins DESC
+LIMIT 10;
+
+--—How many drivers born after 2000 have won a race?
+Experience (Join):
+SELECT *
+FROM drivers
 JOIN driverstandings
-ON results.driverid = driverstandings.driverid
-JOIN constructors
-ON results.constructorid = constructors.constructorid
-JOIN drivers
-ON results.driverid = drivers.driverid;
+ON drivers.driverid = driverstandings.driverid
+WHERE dob > '2000-01-01' AND wins >= 1;
+
+--How often do drivers win the race when they qualify in 1st place?
+
+WITH 
+X1 AS (SELECT COUNT(ds.wins) AS wins FROM driverstandings AS ds
+JOIN qualifying AS q ON ds.driverid = q.driverid
+WHERE q.position = 1 AND ds.wins = 1),
+X2 AS (SELECT COUNT(resultid) AS races FROM results)
+SELECT (CAST(X1.wins AS FLOAT) / CAST(X2.races AS FLOAT)) * 100 AS Pct_Wins_Starting_1st
+FROM X1, X2;
