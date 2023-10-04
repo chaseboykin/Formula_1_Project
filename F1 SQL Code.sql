ec10ae39fad890f9896d76eab0f5469e467759b4
chaseboykin <140556718+chaseboykin@users.noreply.github.com>
2023-10-04 18:01:52 -0500

    Update F1 SQL Code.sql

--- F1 SQL Code.sql
+++ F1 SQL Code.sql
@@ -9,35 +9,14 @@ ON constructorstandings.raceid = races.raceid
 WHERE races.year = 2022
 GROUP by constructors.name, constructors.nationality;
 
+--Who are the most dominant teams of all time?
 
-
---Who has been the most dominant team since 2000?
-
-SELECT constructors.name, constructorstandings.wins
+SELECT constructors.name
 FROM constructors
 JOIN constructorstandings
 ON constructors.constructorid = constructorstandings.constructorid
 JOIN races
 ON constructorstandings.raceid = races.raceid
-WHERE date > '2000-01-01'
-GROUP BY constructors.name, constructorstandings.wins
-ORDER BY constructorstandings.wins DESC
+GROUP BY constructors.name
+ORDER BY SUM(constructorstandings.points) DESC
 LIMIT 10;
-
---How many drivers born in or after 1998 have won a race?
-SELECT forename, surname
-FROM drivers
-JOIN driverstandings
-ON drivers.driverid = driverstandings.driverid
-WHERE dob > '1998-01-01' AND wins >= 1
-GROUP BY forename, surname;
-
---How often do drivers win the race when they qualify in 1st place?
-
-WITH 
-X1 AS (SELECT COUNT(ds.wins) AS wins FROM driverstandings AS ds
-JOIN qualifying AS q ON ds.driverid = q.driverid
-WHERE q.position = 1 AND ds.wins = 1),
-X2 AS (SELECT COUNT(resultid) AS races FROM results)
-SELECT (CAST(X1.wins AS FLOAT) / CAST(X2.races AS FLOAT)) * 100 AS Pct_Wins_Starting_1st
-FROM X1, X2;
